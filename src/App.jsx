aaa832d3c2c95128e68567283c3bad09a2e9c24f
Chaitanya Shah <sendittochaitanya@gmail.com>
2026-01-18 22:12:40 +0530

    Responsive nav layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <>
       <div className='text-gray-600 md:grid grid-cols-5'>
 
-        <div className='md:col-span-1'>
-          <nav>
-            <div>
+        <div className='md:col-span-1 md:flex md:justify-end'>
+          <nav className='text-right'>
+            <div className='flex justify-between items-center'>
               <h1 className='text-red-800 font-bold uppercase p-4 border-b border-gray-100 font-serif'>
                 <a href="/">Food Page</a>
               </h1>
+
+              <div className='px-4 cursor-pointer md:hidden' onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <svg className='w-6' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg>
+              </div>
             </div>
-            <ul>
-              <li className='text-gray-800 font-medium'>
-                <a href='#'><span>Home
-                    <svg className='w-5' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>
+            <ul className={`text-sm mt-6 md:block ${
+              isMenuOpen ? 'block opacity-100' : 'hidden opacity-0'
+            }`} >
+              <li className='text-gray-800 font-medium py-1'>
+                <a href='#' className='px-4 flex justify-end border-r-4 border-rose-400'>
+                  <span>Home
+                    <svg className='w-5 inline-block ml-2' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>
                   </span></a>
               </li>
-              <li>
-                <a href='#'><span>About
-                    <svg className='w-5' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" /></svg>
+              <li className='py-1'>
+                <a href='#' className='px-4 flex justify-end border-r-4 border-white'>
+                  <span>About
+                    <svg className='w-5 inline-block ml-2' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" /></svg>
                   </span></a>
               </li>
-              <li>
-                <a href='#'><span>Contact
-                    <svg className='w-5' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" /></svg>
+              <li className='py-1'>
+                <a href='#' className='px-4 flex justify-end border-r-4 border-white'>
+                  <span>Contact
+                    <svg className='w-5 inline-block ml-2' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" /></svg>
                   </span></a>
               </li>
             </ul>
@@ -91,4 +103,4 @@ function App() {
   )
 }
 
-export default App
+export default App;

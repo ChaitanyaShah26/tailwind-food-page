bad3c423eb718109f6759dcdfbaf9170eec30d84
Chaitanya Shah <sendittochaitanya@gmail.com>
2026-01-18 16:38:14 +0530

    Basic page structure & content

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <nav>
+            <div>
+              <h1>
+                <a href="/">Food Page</a>
+              </h1>
+            </div>
+            <ul>
+              <li>
+                <a href='#'><span>Home</span></a>
+              </li>
+              <li>
+                <a href='#'><span>About</span></a>
+              </li>
+              <li>
+                <a href='#'><span>Contact</span></a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <main>
+          <div>
+            <a href='#'>Log In</a>
+            <a href='#'>Sign Up</a>
+          </div>
+
+          <header>
+            <h2>Recipes</h2>
+            <h3>For Food Lovers</h3>
+          </header>
+
+          <div>
+            <h4>Latest Recipes</h4>
+
+            <div>
+              <div>
+                <img src="" alt=""></img>
+                <div>
+                  <span>Dish Name</span>
+                  <span>Recipe by</span>
+                </div>
+              </div>
+            </div>
+
+            <h4>Most Popular Recipes</h4>
+            <div>
+
+            </div>
+
+
+            <div>
+              <button>Load More</button>
+            </div>
+          </div>
+
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
